438985a56eaa1833be1bdd9afec262307d24579b
Joel Kuehne <joelckuehne@gmail.com>
2024-10-22 08:01:19 -0500

    Changes to nord

--- conf/nvim/lua/plugins/bulk.lua
+++ conf/nvim/lua/plugins/bulk.lua
@@ -4,7 +4,7 @@ return {
 	-- the colorscheme should be available when starting Neovim
 	{
 		"gbprod/nord.nvim",
-		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		lazy = false,
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
 			-- load the colorscheme here
@@ -25,6 +25,10 @@ return {
 	},
 	{
 		"sainnhe/gruvbox-material",
+		event = "VeryLazy"
+	},
+	{
+		"fneu/breezy",
 		event = "VeryLazy"
 	},
 	{
